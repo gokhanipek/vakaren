fc56fcf7b6aab18a2afb3d4dc8324f1e9fc37d18
Gökhan İpek <gokhanipek@GOKHAN-IPEK-MACBOOK-PRO.local>
2020-08-31 22:07:33 +0300

    service worker unregister added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import registerServiceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
+
 import './index.scss';
 
 ReactDOM.render( < App / > , document.getElementById("root"));
-registerServiceWorker();
+
+serviceWorker.unregister();
+
+
